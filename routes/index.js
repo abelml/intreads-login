610558c916ba8410c89c8382736a5db153d9d277
Abel <abel@abelml.eu>
2015-07-12 15:49:26 +0200

    UPDATE Routes to serve local login and sign up

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
+
+var loginController = require('../controllers/loginController.js');
+
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', loginController.index);
+router.get('/login', loginController.login);
+router.get('/signup', loginController.signup);
+router.get('/profile', loginController.loggedIn, loginController.profile);
+router.get('/logout', loginController.logout);
+
+var pass = require('../config/passport.js');
+
+router.post('/login',
+	pass.authenticate('local-login', {
+		successRedirect: '/profile',
+		failureRedirect: '/login',
+		failureFlash: true
+	}),
+	function (req, res) {
+		console.log('Local login done!');
+	}
+);
+
+router.post('/signup',
+	pass.authenticate('local-signup', {
+		successRedirect: '/profile',
+		failureRedirect: '/signup',
+		failureFlash: true
+	}),
+	function (req, res) {
+		console.log('Local sign up done!');
+	}
+);
 
 module.exports = router;
